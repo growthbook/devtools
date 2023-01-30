bd4e84ff0c1b3d5da27d7e2235320404359eedca
bttf <2374625+bttf@users.noreply.github.com>
2023-01-30 10:23:49 -0500

    ExperimentCreator - Create variations

--- src/visual_editor/ExperimentCreator/index.tsx
+++ src/visual_editor/ExperimentCreator/index.tsx
@@ -1,14 +1,16 @@
+import html2canvas from "html2canvas";
 import React, { FC, useEffect, useState } from "react";
 import GripHandle from "../Toolbar/GripHandle";
 // @ts-expect-error ts-loader can't handle png files yet
 import GBLogo from "../../../public/logo192.png";
+import clsx from "clsx";
 
 interface ExperimentVariation {
-  imgUrl: string;
+  canvas?: HTMLCanvasElement;
 }
 
 interface Experiment {
-  variations: ExperimentVariation[];
+  variations?: ExperimentVariation[];
 }
 
 const ExperimentCreator: FC = () => {
@@ -16,28 +18,44 @@ const ExperimentCreator: FC = () => {
   const [x, setX] = useState(24); // pixels
   const [y, setY] = useState(24); // pixels
   const [experiment, setExperiment] = useState<Experiment | null>(null);
+  const [selectedVariationIndex, setSelectedVariationIndex] = useState(1);
+
+  const isEditingControl = selectedVariationIndex === 0;
 
   useEffect(() => {
-    if (!experiment) return;
-
-    if (!experiment?.variations?.length) {
+    const initExperiment = async () => {
+      // TODO DRY
+      const canvas = await html2canvas(document.body, { scale: 0.125 });
       setExperiment({
         ...experiment,
-        // TODO create snapshot of control
-        variations: [{ imgUrl: GBLogo }],
+        variations: [{ canvas }, { canvas }],
       });
-    }
-  }, [experiment]);
+    };
+    initExperiment();
+  }, []);
+
+  const appendVariation = () => {
+    const addVariation = async () => {
+      // TODO DRY
+      const canvas = await html2canvas(document.body, { scale: 0.125 });
+      setExperiment({
+        ...experiment,
+        variations: [...(experiment?.variations ?? []), { canvas }],
+      });
+      setSelectedVariationIndex(experiment?.variations?.length ?? 0);
+    };
+    addVariation();
+  };
 
   return (
     <div
-      className="fixed rounded-xl shadow-xl z-max logo-bg"
+      className="fixed rounded-xl shadow-xl z-max logo-bg w-96 cursor-default"
       style={{
         top: `${y}px`,
         right: `${x}px`,
       }}
     >
-      <div className="flex px-4 h-12 w-96 items-center justify-center">
+      <div className="flex px-4 h-12 items-center justify-center">
         <div className="h-8">
           <img src={GBLogo} alt="GB Logo" className="w-auto h-full mr-1" />
         </div>
@@ -46,11 +64,56 @@ const ExperimentCreator: FC = () => {
         </div>
       </div>
 
-      <div className="relative bg-slate-800 py-4 text-white">
-        <div className="flex justify-center">
-          <button className="p-2 text-xl">+ New Experiment</button>
-        </div>
+      <div className="relative bg-slate-800 py-4 text-white flex justify-center">
+        {experiment ? (
+          <div className="flex flex-wrap justify-start w-10/12">
+            {experiment.variations?.map((variation, i) => (
+              <div
+                key={i}
+                className={clsx(
+                  "relative w-32 h-32 m-4 bg-slate-100 rounded overflow-hidden flex justify-center items-center cursor-pointer",
+                  {
+                    "outline outline-4 outline-amber-300":
+                      i === selectedVariationIndex,
+                  }
+                )}
+                onClick={() => setSelectedVariationIndex(i)}
+              >
+                <img
+                  className="min-w-full min-h-full object-cover"
+                  src={variation.canvas?.toDataURL()}
+                  alt="Variation"
+                />
+                <div
+                  className={clsx(
+                    "absolute inset-0 text-xl font-semibold flex justify-center items-center",
+                    {
+                      "bg-slate-500/75": i !== selectedVariationIndex,
+                      "text-slate-700": i === selectedVariationIndex,
+                      "text-white": i !== selectedVariationIndex,
+                    }
+                  )}
+                >
+                  {i === 0 ? "Control" : `Variation ${i}`}
+                </div>
+              </div>
+            ))}
+            <div
+              className="w-32 h-32 m-4 border-4 border-slate-300 border-dashed rounded flex items-center text-slate-300 text-xl font-semibold text-center cursor-pointer hover:border-slate-100 hover:text-slate-100"
+              onClick={() => appendVariation()}
+            >
+              + Add Variation
+            </div>
+          </div>
+        ) : (
+          <div className="flex justify-center">
+            <button className="p-2 text-xl" onClick={() => setExperiment({})}>
+              + New Experiment
+            </button>
+          </div>
+        )}
       </div>
+
       <GripHandle
         reverseX
         className="bg-slate-800 h-5 w-full rounded-b-xl"
