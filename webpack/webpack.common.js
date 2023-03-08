baa705be119d7b9b4e478d7cddd8637038a2617a
tinahollygb <tina@growthbook.io>
2023-03-08 14:41:38 -0800

    add plop to generate components

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -29,7 +29,10 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: "ts-loader",
-        exclude: /node_modules/,
+        exclude: [
+          /node_modules/,
+          /\.stories\.(ts|tsx)$/
+        ],
       },
       {
         test: /\.css$/,
