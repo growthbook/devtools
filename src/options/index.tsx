5e7d746e0579e8e96fe1e63a1ffb0a129d3c47f7
bttf <2374625+bttf@users.noreply.github.com>
2023-03-31 13:57:48 -0400

    Remove unnecessary chunks config

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import * as ReactDOM from "react-dom/client";
 import { useApiKey } from "../utils/hooks";
 import ApiKeyForm from "./ApiKeyForm";
