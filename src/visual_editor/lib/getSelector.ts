8a1309bd5844164a51633e8203f975cab2cbefa6
bttf <2374625+bttf@users.noreply.github.com>
2023-04-17 15:33:39 -0400

    Remove classname blocker from finder util

--- src/visual_editor/lib/getSelector.ts
+++ src/visual_editor/lib/getSelector.ts
@@ -3,7 +3,5 @@ import { finder } from "@medv/finder";
 export default function getSelector(element: Element) {
   return finder(element, {
     seedMinLength: 5,
-    // ignore class names since they can be generated randomly with some libraries (styled-compoonents, css modules)
-    className: (_name) => false,
   });
 }
