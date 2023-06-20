34e5e4c08d073820638af57ab96a3a770af48e3f
bttf <2374625+bttf@users.noreply.github.com>
2023-06-20 15:12:48 -0700

    Get to a good state with borders

--- src/visual_editor/AICopySuggestor.tsx
+++ src/visual_editor/AICopySuggestor.tsx
@@ -70,7 +70,7 @@ const AICopySuggestor: FC<{
       <div className="gb-w-24 gb-text-xs gb-text-slate-400">Original</div>
       <div
         className={clsx(
-          "gb-text-slate-200",
+          "gb-text-slate-400",
           "gb-text-ellipsis",
           "gb-overflow-hidden",
           "gb-text-sm",
@@ -78,25 +78,48 @@ const AICopySuggestor: FC<{
           "gb-mr-4",
           "gb-mt-2",
           "gb-p-2",
-          "gb-rounded"
+          "gb-rounded",
+          "gb-border",
+          "gb-border-slate-700",
+          "gb-z-20"
         )}
         style={{ flex: 2, maxHeight: "3rem" }}
       >
         {copy}
       </div>
 
-      <div className="gb-w-24 gb-text-xs gb-text-slate-400 gb-mt-2">
+      <div className="gb-w-24 gb-text-xs gb-text-slate-400 gb-mt-2 gb-w-full gb-relative">
         Make it more...
+        <div
+          className="gb-z-10 -gb-top-2 gb-bottom-0 gb-inset-x-1/2 gb-absolute gb-h-6 gb-border-l gb-border-slate-700 gb-h-9"
+          style={{ width: "1px" }}
+        ></div>
       </div>
-      <div className="gb-flex gb-mr-4 gb-mt-2 gb-text-sm">
-        <div className="gb-p-1 gb-flex-1 gb-flex gb-items-center gb-justify-center">
-          concise
+
+      <div className="gb-flex gb-mr-4 gb-mt-2 gb-text-sm gb-h-7 gb-z-20">
+        <div className="gb-flex-1 gb-flex gb-logo-bg gb-rounded-l">
+          <div
+            className="gb-bg-slate-700 gb-flex-1 gb-flex gb-justify-center gb-items-center gb-rounded-l"
+            style={{ margin: "1px" }}
+          >
+            concise
+          </div>
         </div>
-        <div className="gb-p-1 gb-flex-1 gb-flex gb-items-center gb-justify-center">
-          energetic
+        <div className="gb-flex-1 gb-flex gb-bg-slate-700">
+          <div
+            className="gb-bg-slate-900 gb-flex-1 gb-flex gb-justify-center gb-items-center"
+            style={{ margin: "1px" }}
+          >
+            energetic
+          </div>
         </div>
-        <div className="gb-p-1 gb-flex-1 gb-flex gb-items-center gb-justify-center">
-          humorous
+        <div className="gb-flex-1 gb-flex gb-bg-slate-700 gb-rounded-r">
+          <div
+            className="gb-bg-slate-900 gb-flex-1 gb-flex gb-justify-center gb-items-center gb-rounded-r"
+            style={{ margin: "1px" }}
+          >
+            humorous
+          </div>
         </div>
       </div>
 
