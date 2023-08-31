487dbe7d20a82c824d0a43204730b7ee70326c74
bttf <2374625+bttf@users.noreply.github.com>
2023-08-31 15:02:36 -0700

    Use parse-domain

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,10 @@
+import { isEqual } from "lodash";
+import {
+  fromUrl,
+  parseDomain,
+  ParseResultType,
+  Validation,
+} from "parse-domain";
 import {
   APIExperiment,
   APIVisualChangeset,
@@ -225,11 +232,39 @@ const transformCopy = async ({
 
 const isSameOrigin = (url: string, origin: string) => {
   try {
-    const urlHostname = new URL(url).hostname;
-    const originHostname = new URL(origin).hostname;
-    const urlMainDomain = urlHostname.split(".").slice(-2).join(".");
-    const originMainDomain = originHostname.split(".").slice(-2).join(".");
-    return urlMainDomain === originMainDomain;
+    const urlParseResult = parseDomain(fromUrl(url), {
+      validation: Validation.Lax,
+    });
+    const originParseResult = parseDomain(fromUrl(origin), {
+      validation: Validation.Lax,
+    });
+    if (
+      urlParseResult.type === ParseResultType.Listed &&
+      originParseResult.type === ParseResultType.Listed
+    ) {
+      const { domain: urlDomain } = urlParseResult;
+      const { domain: originDomain } = originParseResult;
+
+      return urlDomain === originDomain;
+    } else if (
+      urlParseResult.type === ParseResultType.Reserved &&
+      originParseResult.type === ParseResultType.Reserved
+    ) {
+      return isEqual(urlParseResult.labels, originParseResult.labels);
+    } else if (
+      urlParseResult.type === ParseResultType.NotListed &&
+      originParseResult.type === ParseResultType.NotListed
+    ) {
+      return isEqual(urlParseResult.labels, originParseResult.labels);
+    } else {
+      console.error('Unrecognizable domain type for either "url" or "origin"', {
+        url,
+        origin,
+      });
+      throw new Error(
+        'Unrecognizable domain type for either "url" or "origin"'
+      );
+    }
   } catch (e) {
     console.error("isSameOrigin - Error checking origin", e);
     return false;
