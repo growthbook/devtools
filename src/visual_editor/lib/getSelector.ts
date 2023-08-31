75ee5b357b0dec34eb145c7f6072375371d32ac7
bttf <2374625+bttf@users.noreply.github.com>
2023-08-31 14:40:44 -0700

    Fix

--- src/visual_editor/lib/getSelector.ts
+++ src/visual_editor/lib/getSelector.ts
@@ -3,7 +3,10 @@ import { REARRANGE_CLASSNAME_PREFIX } from "./hooks/useRearrangeMode";
 
 export default function getSelector(element: Element) {
   // TODO - this is a hack - we should use the finder library to find the selector
-  if (element.className?.includes(REARRANGE_CLASSNAME_PREFIX)) {
+  if (
+    element.className &&
+    element.className.includes?.(REARRANGE_CLASSNAME_PREFIX)
+  ) {
     const className = element.className
       .split(" ")
       .find((c) => c.startsWith(REARRANGE_CLASSNAME_PREFIX));
