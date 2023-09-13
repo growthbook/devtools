016ac9a408f122cec6b6a6b39afe8130abba3285
bttf <2374625+bttf@users.noreply.github.com>
2023-09-12 20:09:26 -0700

    Add job

---
+++ .github/scripts/check-pkg-version.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+async function checkVersion() {
+  const packageName = "@growthbook/growthbook"; // Replace this with the package name you want to check
+  const packageJson = JSON.parse(fs.readFileSync("package.json"));
+  const localVersion =
+    packageJson.dependencies[packageName] ||
+    packageJson.devDependencies[packageName];
+
+  const response = await fetch(`https://registry.npmjs.org/${packageName}`);
+  const data = await response.json();
+  const latestVersion = data["dist-tags"].latest;
+
+  if (localVersion !== latestVersion) {
+    console.error(
+      `The local version ${localVersion} is not the latest version ${latestVersion}.`
+    );
+    process.exit(1);
+  }
+}
+
+checkVersion();
