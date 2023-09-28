8d19e7b5afcc86a981efe88a80f4cb7d1544ba7e
Adnan Chowdhury <2374625+bttf@users.noreply.github.com>
2023-09-28 15:36:52 -0400

    Upgrade @growthbook/growthbook (#38)

---
+++ .github/scripts/check-pkg-version.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+async function checkVersion() {
+  const packageName = "@growthbook/growthbook"; // Replace this with the package name you want to check
+  const packageJson = JSON.parse(fs.readFileSync("package.json"));
+  const localVersion = packageJson.dependencies[packageName];
+
+  const response = await fetch(`https://registry.npmjs.org/${packageName}`);
+  const data = await response.json();
+  const latestVersion = data["dist-tags"].latest;
+
+  if (localVersion.replace(/\^/, "") !== latestVersion) {
+    console.error(
+      `The local version of @growthbook/growthbook (${localVersion}) is not the latest version ${latestVersion}.`
+    );
+    process.exit(1);
+  }
+}
+
+checkVersion();
