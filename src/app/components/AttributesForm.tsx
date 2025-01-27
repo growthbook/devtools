f45523f09d7d23c4b49618f718e84351c5088306
Bryce <bryce1@gmail.com>
2025-01-27 13:27:09 -0800

    mutate fields

--- src/app/components/AttributesForm.tsx
+++ src/app/components/AttributesForm.tsx
@@ -1,31 +1,63 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import * as Form from "@radix-ui/react-form";
-import { Switch } from "@radix-ui/themes";
+import {Button, Switch} from "@radix-ui/themes";
 import { Attributes } from "@growthbook/growthbook";
 import {UseFormReturn} from "react-hook-form";
+import {PiXCircle} from "react-icons/pi";
 
 export default function AttributesForm({
   form,
   jsonMode = false,
   schema,
+  canAddRemoveFields = true,
 }: {
   form: UseFormReturn<Attributes>;
   jsonMode?: boolean;
   schema?: Record<string, string>;
+  canAddRemoveFields?: boolean;
 }) {
   const attributes = form.getValues();
+  const [fields, setFields] = useState(new Set(Object.keys(attributes)));
+
+  const addField = (key: string) => {
+    setFields(prev => new Set([...prev, key]));
+  }
+  const removeField = (key: string) => {
+    form.setValue(key, undefined);
+    setFields(prev => new Set([...prev].filter(x => x !== key)));
+  };
+
+  useEffect(() => {
+    setFields(new Set(Object.keys(attributes)));
+  }, [JSON.stringify(form.getValues())]);
 
   return (
     <>
       <div>
         <Form.Root className="FormRoot small">
           {!jsonMode ?
-            Object.keys(attributes).map((a, i) => {
+            [...fields].map((attributeKey, i) => {
               return (
-                <div key={a}>
-                  <Form.Field className="FormFieldInline my-1" name={a}>
-                    <Form.Label className="FormLabel mr-1" style={{ minWidth: 80 }}>{a}</Form.Label>
-                    {renderInputField(a, form, schema)}
+                <div key={attributeKey}>
+                  <Form.Field className="FormFieldInline my-1" name={attributeKey}>
+                    <Form.Label className="FormLabel mr-1 text-nowrap">
+                      {canAddRemoveFields && (
+                        <Button
+                          type="button"
+                          size="1"
+                          variant="ghost"
+                          color="red"
+                          className="px-1 mt-0.5 mr-0.5"
+                          onClick={() => removeField(attributeKey)}
+                        >
+                          <PiXCircle />
+                        </Button>
+                      )}
+                      <div className="inline-block -mb-2 overflow-hidden overflow-ellipsis" style={{ minWidth: 80, maxWidth: 120 }}>
+                        {attributeKey}
+                      </div>
+                    </Form.Label>
+                    {renderInputField({ attributeKey, form, schema })}
                   </Form.Field>
                 </div>
               );
@@ -45,18 +77,26 @@ export default function AttributesForm({
   );
 }
 
-function renderInputField(a: string, form: UseFormReturn<Attributes>, schema?: Record<string, string>) {
-  let attributeType = getAttributeType(a, form.watch(a), schema);
+function renderInputField({
+  attributeKey,
+  form,
+  schema,
+}: {
+  attributeKey: string;
+  form: UseFormReturn<Attributes>;
+  schema?: Record<string, string>;
+}) {
+  let attributeType = getAttributeType(attributeKey, form.watch(attributeKey), schema);
   // todo: enum, number[], string[]
   // todo (maybe. or just use string): secureString, secureString[]
   return (
     <Form.Control asChild>
       {attributeType === "number" ? (
-        <input className="Input" type="number" {...form.register(a)} />
+        <input className="Input" type="number" {...form.register(attributeKey)} />
       ): attributeType === "boolean" ? (
-        <Switch size="2" className="Switch" checked={form.watch(a)} onCheckedChange={(v: boolean) => form.setValue(a, v)} />
+        <Switch size="2" className="Switch" checked={form.watch(attributeKey)} onCheckedChange={(v: boolean) => form.setValue(attributeKey, v)} />
       ) : (
-        <input className="Input" {...form.register(a)} />
+        <input className="Input" {...form.register(attributeKey)} />
       )}
     </Form.Control>
   );
