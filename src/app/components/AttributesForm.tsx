ed9d6867aac9b2c336b1b3de0ec89bc32b08342d
Bryce <bryce1@gmail.com>
2025-01-27 17:34:00 -0800

    add custom attribute fields

--- src/app/components/AttributesForm.tsx
+++ src/app/components/AttributesForm.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import * as Form from "@radix-ui/react-form";
-import {Button, Switch, Link} from "@radix-ui/themes";
+import {Button, Switch, Link, Select} from "@radix-ui/themes";
 import { Attributes } from "@growthbook/growthbook";
 import {UseFormReturn} from "react-hook-form";
-import {PiXCircle} from "react-icons/pi";
+import {PiPlusCircle, PiXCircle} from "react-icons/pi";
 import useTabState from "@/app/hooks/useTabState";
 
 export default function AttributesForm({
@@ -22,9 +22,12 @@ export default function AttributesForm({
   canAddRemoveFields?: boolean;
 }) {
   const [attributes, setAttributes] = useTabState<Attributes>("attributes", {});
+  const [addingCustom, setAddingCustom] = useState(false);
+  const [addCustomId, setAddCustomId] = useState("");
+  const [addCustomType, setAddCustomType] = useState("string");
   const formAttributes = form.getValues();
 
-  const addField = (key: string) => {
+  const addField = (key: string, type: string) => {
     setDirty?.(true);
     const newAttributes = form.getValues();
     newAttributes[key] = undefined;
@@ -36,6 +39,21 @@ export default function AttributesForm({
     delete newAttributes?.[key];
     form.reset(newAttributes);
   };
+  const addCustomField = () => {
+    setDirty?.(true);
+    const newAttributes = form.getValues();
+    newAttributes[addCustomId] = addCustomType === "number" ? 0 : addCustomType === "boolean" ? false : "string";
+    // todo: number[], string[]
+    form.reset(newAttributes);
+    setAddingCustom(false);
+    setAddCustomId("");
+    setAddCustomType("string");
+  }
+  const cancelAddCustomField = () => {
+    setAddingCustom(false);
+    setAddCustomId("");
+    setAddCustomType("string");
+  }
   const applyAttributes = () => {
     setAttributes(formAttributes);
     form.reset(formAttributes);
@@ -71,19 +89,20 @@ export default function AttributesForm({
                           className="px-1 mt-0.5 mr-0.5"
                           onClick={() => removeField(attributeKey)}
                         >
-                          <PiXCircle />
+                          <PiXCircle/>
                         </Button>
                       )}
-                      <div className="inline-block -mb-2 overflow-hidden overflow-ellipsis" style={{ minWidth: 80, maxWidth: 120 }}>
+                      <div className="inline-block -mb-2 overflow-hidden overflow-ellipsis"
+                           style={{minWidth: 80, maxWidth: 120}}>
                         {attributeKey}
                       </div>
                     </Form.Label>
-                    {renderInputField({ attributeKey, form, schema })}
+                    {renderInputField({attributeKey, form, schema})}
                   </Form.Field>
                 </div>
               );
-          })
-          : (
+            })
+            : (
               <textarea
                 className="Textarea mono"
                 name={"__JSON_attributes__"}
@@ -91,6 +110,58 @@ export default function AttributesForm({
                 value={JSON.stringify(formAttributes, null, 2)}
                 rows={12}
               />
+            )}
+          {canAddRemoveFields && (
+            <div className="mt-2">
+              {/*todo: map through unused attributes from schema*/}
+              {!addingCustom ? (
+                <div key="add_custom">
+                  <Button
+                    type="button"
+                    size="2"
+                    variant="ghost"
+                    className="px-1 mt-0.5 mr-0.5"
+                    onClick={() => {
+                      setAddingCustom(true);
+                    }}
+                  >
+                    <PiPlusCircle /> Add {schema ? "custom" : ""}attribute...
+                  </Button>
+                </div>
+              ) : (
+                <div key="add_custom" className="mt-1 flex justify-between items-center" style={{ fontSize: 12 }}>
+                  <div className="flex items-center gap-1 mr-3">
+                    <span>Add</span>
+                    <input placeholder="field name" className="Input" value={addCustomId} onChange={(e) => {
+                      const v = e.target.value;
+                      setAddCustomId(v);
+                    }}/>
+                    <Select.Root defaultValue="string" size="1" value={addCustomType}
+                                 onValueChange={(v) => setAddCustomType(v)}>
+                      <Select.Trigger className="uppercase">
+                        <div className="text-green-700">
+                          &lt;{addCustomType?.[0] || "S"}&gt;
+                        </div>
+                      </Select.Trigger>
+                      <Select.Content>
+                        <Select.Item value="string">String</Select.Item>
+                        <Select.Item value="number">Number</Select.Item>
+                        <Select.Item value="boolean">Boolean</Select.Item>
+                        {/*todo: number[], string[]*/}
+                      </Select.Content>
+                    </Select.Root>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <Button disabled={!addCustomId.trim()} type="button" size="1" onClick={() => addCustomField()}>
+                      Add
+                    </Button>
+                    <Button type="button" size="1" color="gray" variant="outline" onClick={() => cancelAddCustomField()}>
+                      X
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </div>
           )}
           {dirty && (
             <div className="mt-2 flex justify-between items-center">
@@ -105,7 +176,7 @@ export default function AttributesForm({
         </Form.Root>
       </div>
     </>
-  );
+);
 }
 
 function renderInputField({
