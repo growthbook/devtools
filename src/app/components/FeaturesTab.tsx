7b25790d0a3ec95a6f8324eccc968d640d905f30
Bryce <bryce1@gmail.com>
2025-02-14 16:06:01 -0800

    ui

--- src/app/components/FeaturesTab.tsx
+++ src/app/components/FeaturesTab.tsx
@@ -215,8 +215,7 @@ export default function FeaturesTab() {
                 >
                 <div
                     className={clsx("title absolute line-clamp-1 pl-2.5 pr-3", {
-                      "top-1": !fullWidthListView,
-                      "top-[25%]": fullWidthListView,
+                      "top-1 text-right": !fullWidthListView,
                     })}
                     style={{ width: fullWidthListView ? col1 : undefined }}
                   >
